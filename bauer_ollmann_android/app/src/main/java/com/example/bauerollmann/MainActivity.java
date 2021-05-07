76569c37c01fd802d1bdf4778cb847c8aa9e4bc7
Sascha Bauer <sascha@bauercloud.at>
2021-05-07 08:47:24 +0200

    added some code in MainActivity, Member1

--- bauer_ollmann_android/app/src/main/java/com/example/bauerollmann/MainActivity.java
+++ bauer_ollmann_android/app/src/main/java/com/example/bauerollmann/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        int gitTest = "Neuer Eintrag in einer Funktion für GIT."
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
